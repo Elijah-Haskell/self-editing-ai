a95b56812fad8845f67c0e82a9f4f82df188d891
Elijah-Haskell <elijahphask@gmail.com>
2025-08-13 10:29:25 -0700

    Create agent/__init__.py

---
+++ src/agent/__init__.py
@@ -0,0 +1,8 @@
+"""Self-editing AI agent.
+
+This package provides the AgentLoop class to run self-editing AI tasks, along with associated utilities.
+"""
+
+from .loop import AgentLoop
+
+__all__ = ["AgentLoop"]
